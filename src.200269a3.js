0b08d0dafe21312a62fa55d48f6ab236d425fe68
TyomaDW <81493194+TyomaDW@users.noreply.github.com>
2021-08-13 18:06:14 +0000

    Deploying to gh-pages from @ TyomaDW/team-project-group-8@2b95ebd5f1fd9e5813ef01c260aa0db28cd628a9 🚀

--- src.200269a3.js
+++ src.200269a3.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/team-project-group-8/src.1ad6e44e.js.map
+//# sourceMappingURL=/team-project-group-8/src.200269a3.js.map
